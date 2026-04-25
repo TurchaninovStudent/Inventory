026acc1e3532299d2dcdd9f8cee1c4d0939c14a6
TurchaninovStudent <aeturchaninov@ya.ru>
2026-04-25 09:37:42 +0300

    Создан класс InventoryDatabase

---
+++ app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
@@ -0,0 +1,26 @@
+package com.example.inventory.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Item::class], version = 1, exportSchema = false)
+abstract class InventoryDatabase : RoomDatabase() {
+    abstract fun ItemDao(): ItemDao
+
+    companion object {
+        @Volatile
+        private var Instance: InventoryDatabase? = null
+
+        fun GetDataBase(context: Context): InventoryDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, InventoryDatabase::class.java, "item_database")
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+
+}
