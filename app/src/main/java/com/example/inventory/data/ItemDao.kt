10e5c0379b58f73824298870ccd95bbf2e2dd14c
TurchaninovStudent <aeturchaninov@ya.ru>
2026-04-25 09:30:10 +0300

    Реализован ItemDao с операциями CRUD и Flow-запросами

---
+++ app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -0,0 +1,27 @@
+package com.example.inventory.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ItemDao {
+    @Query("SELECT * FROM items WHERE id = :id")
+    fun GetItemById(id: Int): Flow<Item>
+
+    @Query("SELECT * FROM items ORDER BY name ASC ")
+    fun GetAllItems(): Flow<List<Item>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun Insert(item: Item)
+
+    @Update
+    suspend fun Update(item: Item)
+
+    @Delete
+    suspend fun Delete(item: Item)
+}
